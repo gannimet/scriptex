d85afa81e9b36b8d48a7a334449b23df138fbb4f
gannimet <r.wotzlaw@gmail.com>
2014-02-25 10:58:03 +0100

    outsourced constans

--- test/indents.js
+++ test/indents.js
@@ -1,14 +1,15 @@
 var should = require('should');
 var scriptex = require('../../scriptex');
+var constants = require('../lib/constants');
 
 describe('Indents', function() {
 	describe('Getting indents', function() {
 		it('getting scalar values', function() {
-			scriptex.getIndent('fadein').should.be.exactly(15);
+			scriptex.getIndent(constants.FADEIN).should.be.exactly(15);
 			scriptex.getIndent('hurz').should.be.false;
 		});
 		it('getting object values', function() {
-			scriptex.getIndent('transition').should.eql({
+			scriptex.getIndent(constants.TRANSITION).should.eql({
 				minValue: 60,
 				maxValue: 75
 			});
@@ -17,51 +18,72 @@ describe('Indents', function() {
 
 	describe('Setting indents', function() {
 		it('setting scalar values', function() {
-			scriptex.setIndent('character', 35).should.be.true;
-			scriptex.getIndent('character').should.be.exactly(35);
+			scriptex.setIndent(constants.CHARACTER, 35).should.be.true;
+			scriptex.getIndent(constants.CHARACTER).should.be.exactly(35);
 		});
 		it('setting value as object', function() {
-			scriptex.setIndent('character', {
+			scriptex.setIndent(constants.CHARACTER, {
 				value: 36
 			}).should.be.true;
-			scriptex.getIndent('character').should.be.exactly(36);
+			scriptex.getIndent(constants.CHARACTER).should.be.exactly(36);
 
-			scriptex.setIndent('character', {
+			scriptex.setIndent(constants.CHARACTER, {
 				minValue: 126,
 				value: 130,
 				maxValue: 135
 			}).should.be.true;
-			scriptex.getIndent('character').should.be.exactly(130);
+			scriptex.getIndent(constants.CHARACTER).should.be.exactly(130);
 		});
 		it('setting min and max values as objects', function() {
-			scriptex.setIndent('action', {
+			scriptex.setIndent(constants.ACTION, {
 				minValue: 20,
 				maxValue: 25
 			}).should.be.true;
-			scriptex.getIndent('action').should.eql({
+			scriptex.getIndent(constants.ACTION).should.eql({
 				minValue: 20,
 				maxValue: 25
 			});
 		});
 		it('setting invalid values', function() {
-			scriptex.setIndent('dialogue', 'hurz').should.be.false;
-			scriptex.getIndent('dialogue').should.be.exactly(25);
+			scriptex.setIndent(constants.DIALOGUE, 'hurz').should.be.false;
+			scriptex.getIndent(constants.DIALOGUE).should.be.exactly(25);
 
-			scriptex.setIndent('dialogue', {
+			scriptex.setIndent(constants.DIALOGUE, {
 				minValue: 22,
 				maxValue: 'hurz'
 			}).should.be.true;
 
-			var dialogueIndent = scriptex.getIndent('dialogue');
+			var dialogueIndent = scriptex.getIndent(constants.DIALOGUE);
 			dialogueIndent.minValue.should.be.exactly(22);
 			should.not.exist(dialogueIndent.maxValue);
 
-			scriptex.setIndent('dialogue', 25).should.be.true;
-			scriptex.setIndent('dialogue', {
+			scriptex.setIndent(constants.DIALOGUE, 25).should.be.true;
+			scriptex.setIndent(constants.DIALOGUE, {
 				minValue: '10',
 				maxValue: undefined
 			}).should.be.false;
-			scriptex.getIndent('dialogue').should.be.exactly(25);
+			scriptex.getIndent(constants.DIALOGUE).should.be.exactly(25);
+
+			scriptex.setIndent(constants.ACTION, {
+				value: 'hurz'
+			}).should.be.false;
+			scriptex.setIndent(constants.ACTION, {
+				value: '10.5'
+			}).should.be.false;
+		});
+		it('setting NaN and Infinity', function() {
+			scriptex.setIndent(constants.ACTION, '10.5').should.be.false;
+			scriptex.setIndent(constants.ACTION, '10').should.be.false;
+			scriptex.setIndent(constants.SLUGLINE, NaN).should.be.false;
+			scriptex.setIndent(constants.SLUGLINE, Infinity).should.be.false;
+
+			scriptex.setIndent(constants.CHARACTER, {
+				value: NaN
+			}).should.be.false;
+			scriptex.setIndent(constants.CHARACTER, {
+				minValue: NaN,
+				maxValue: Infinity
+			}).should.be.false;
 		});
 	});
 });
